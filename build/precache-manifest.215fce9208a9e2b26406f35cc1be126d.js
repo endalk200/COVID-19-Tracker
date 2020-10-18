0a2bb4cfbea2676e74ae653d0e63658776e4b5fe
endalk2 <eb808826@gmail.com>
2020-10-18 14:05:07 +0300

    deployed built assets

---
+++ build/precache-manifest.215fce9208a9e2b26406f35cc1be126d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be819528c717a3e9b3f6c95e854c0493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3eff2d89a2a2f12b89b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d5f2f7d29881fd6f257",
+    "url": "/static/js/2.853f01dd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.853f01dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3eff2d89a2a2f12b89b",
+    "url": "/static/js/main.15a49d45.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
